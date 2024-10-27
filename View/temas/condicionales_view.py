9995e796b253ba98b6e53cd9af16ed3c4dcede83
Esteban Arias <carlos.caro199987@gmail.com>
2024-10-26 22:13:20 -0500

    modificacion .gitignore

--- View/temas/condicionales_view.py
+++ View/temas/condicionales_view.py
@@ -5,17 +5,24 @@ def mostrar_contenido_condicionales(parent_frame):
     """Muestra el contenido del tema Variables en el frame principal"""
 
     # Título del tema
-    tk.Label(parent_frame, text="Variables en Python", font=("Arial", 16, "bold")).pack(pady=10)
+    tk.Label(parent_frame, text="Condicionales en Python", font=("Arial", 16, "bold")).pack(pady=10)
 
     # Descripción del tema
     descripcion = (
-        "En Python, una variable se utiliza para almacenar información y puede cambiar su valor durante la ejecución del programa. "
-        "No es necesario declarar el tipo de dato de la variable explícitamente, ya que Python lo infiere según el valor que se le asigne. "
-        "Aquí hay un ejemplo:\n\n"
+        "En Python, los condicionales permiten tomar decisiones en el flujo de un programa, ejecutando ciertas secciones de código solo cuando se cumplen ciertas condiciones. Estos condicionales se construyen principalmente con las estructuras if, elif y else. "
+        "Aquí diferentes ejemplos:\n\n"
+        "Para el condicional if\n\n"
         "x = 5  # Asigna el valor 5 a la variable 'x'\n"
-        "nombre = 'Juan'  # Asigna una cadena de texto a la variable 'nombre'\n"
-        "pi = 3.1416  # Asigna un número decimal a la variable 'pi'\n\n"
-        "Las variables en Python son dinámicas y no requieren una declaración previa con su tipo."
+        "if x > 5:   # Crea la condicion (Si x mayor que 5)'\n"
+        "    print('x es mayor que 5')\n\n"
+        "Para el condicional elif \n\n"
+        "Este se utiliza una vez finaliza el condicional if, no puede utilizarse como principal o independiente, siempre dependera del condicional if como se muestra en el ejemplo. \n\n"
+        "x = 5  # Asigna el valor 5 a la variable 'x'\n"
+        "if x > 5:   # Crea la condicion (Si x mayor que 5)'\n"
+        "    print('x es mayor que 5')  #Imprime el mensaje solo si la condicion es verdadera\n"
+        "elif:  #Declaramos la condicion elif\n"
+        "    print('x es menor que 5')   #En caso de no cumplir la condicion if, imprime su respectiva respuesta"
+        
     )
     tk.Label(parent_frame, text=descripcion, wraplength=550, justify="left").pack(pady=10)
 
@@ -23,14 +30,17 @@ def mostrar_contenido_condicionales(parent_frame):
     ejemplo_codigo = (
         "Ejemplo de uso:\n"
         "a = 10\n"
-        "b = 20\n"
-        "resultado = a + b\n"
-        "print(f'La suma de {{a}} y {{b}} es {{resultado}}')"
+        "if (a<10):\n"
+        "    print('El valor de a es menor a 10)\n"
+        "else:\n"
+        "    print('El valor de a es mayor que 10)"
     )
     tk.Label(parent_frame, text=ejemplo_codigo, font=("Courier", 10), justify="left").pack(pady=10)
 
     # Formulario de selección múltiple
     quiz(parent_frame)
+
+
 
 def quiz(parent_frame):
     """Formulario de selección múltiple para el tema Variables"""
@@ -42,10 +52,10 @@ def quiz(parent_frame):
     var.set('')
 
     opciones = [
-        ("1. x == 10", "incorrecto"),
+        ("1. x == 10", "incorrecto1"),
         ("2. x = 10", "correcto"),
-        ("3. x := 10", "incorrecto"),
-        ("4. 10 = x", "incorrecto")
+        ("3. x := 10", "incorrecto2"),
+        ("4. 10 = x", "incorrecto3")
     ]
 
     for texto, valor in opciones:
@@ -56,7 +66,11 @@ def quiz(parent_frame):
 
 def verificar_respuesta(var):
     """Verifica si la respuesta del cuestionario es correcta"""
-    if var.get() == "correcto":
+    if var.get() == "":
+        messagebox.showwarning("Resultado","Selecciona una respuesta primero")
+    elif var.get() == "correcto":
         messagebox.showinfo("Resultado", "¡Correcto! La asignación de variables en Python es 'x = valor'.")
     else:
-        messagebox.showwarning("Resultado", "Incorrecto. La respuesta correcta es 'x = valor'.")
+        messagebox.showwarning("Resultado", "Incorrecto. Intentelo nuevamente")
+        
+
